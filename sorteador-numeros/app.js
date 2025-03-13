287b0c7d40d2fdef3fc512b6d945ba0ada717cd5
Luis Felipe de Oliveira Monteiro <luisfelipeomonteiro@gmail.com>
2025-03-12 22:42:39 -0300

    Primeira aula de lógica de programação

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,8 @@
+function sortear(){
+    let quantidade = document.getElementById('quantidade').value;
+    let minimo = document.getElementById('de').value;
+    let maximo = document.getElementById('ate').value;
+    let numeros_sorteados = []
+
+    //alert(`Quantidade: ${quantidade}\nMinimo: ${minimo}\nMaximo: ${maximo}`)
+}
