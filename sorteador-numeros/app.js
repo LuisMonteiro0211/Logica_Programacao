82cf5744667553837ba512e3f560645102fed5f0
Luis Felipe de Oliveira Monteiro <luisfelipeomonteiro@gmail.com>
2025-03-12 23:23:33 -0300

    Adiciona função de sorteio de números e correção na declaração de variáveis

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,8 +1,19 @@
 function sortear(){
-    let quantidade = document.getElementById('quantidade').value;
-    let minimo = document.getElementById('de').value;
-    let maximo = document.getElementById('ate').value;
-    let numeros_sorteados = []
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let minimo = parseInt(document.getElementById('de').value);
+    let maximo = parseInt(document.getElementById('ate').value);
+    
+    alert(sorteador_numeros(minimo, maximo, quantidade));
 
     //alert(`Quantidade: ${quantidade}\nMinimo: ${minimo}\nMaximo: ${maximo}`)
 }
+
+function sorteador_numeros(minimo, maximo, quantidade){
+    let numeros_sorteados = [];
+
+    for (let contador = 0; contador <= quantidade; contador++){
+        numeros_sorteados.push(Math.round(Math.random() * (maximo - minimo))+minimo);
+    }
+
+}
+
